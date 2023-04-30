3f7d8a2054e5cb80d3f4afb5a74fa155eb25ff47
nekoura <a.aaura@ya.ru>
2023-04-30 14:28:09 +0300

    Add a model Post

--- site_project/blog/models.py
+++ site_project/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    """
+    Модель, позволяющая хранить посты блога в базе данных. 
+    * `title` - поле заголовка поста, тип `CharField`, транслируемый в столбец `VARCHAR` в SQL
+    * `slug` - поле короткой метки, тип `SlugField`, транслируемый в столбец `VARCHAR` в SQL
+    * `body` - поле тела поста, тип `TextField`, транслируемый в столбец `Text` в SQL
+    """
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
