368f8374e54a222ef3e92a3489b695598e18047d
nekoura <a.aaura@ya.ru>
2023-05-01 13:47:52 +0300

    Add exceptions for paginator

--- site_project/blog/views.py
+++ site_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
@@ -12,7 +12,14 @@ def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, NUM_POSTS)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # При обращении к странице с не целым числом - выдача первой страницы
+        posts = paginator.page(1)
+    except EmptyPage:
+        # При обращении вне диапозона - выдача последней страницы
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
